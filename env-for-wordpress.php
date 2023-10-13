80094442a95d2670170d832c2d2ac4b7728beecd
Alex Yokisama <alex.yokisama@gmail.com>
2023-10-13 23:55:06 +0300

    Initial commit

---
+++ env-for-wordpress.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ *
+ * @since             1.0.0
+ * @package           Env_For_Wordpress
+ *
+ * @wordpress-plugin
+ * Plugin Name:       Env for WordPress
+ * Description:       Env for WordPress
+ * Version:           1.0.0
+ * Author:            Alex Yokisama
+ */
+
+// If this file is called directly, abort.
+if (!defined('WPINC')) {
+	die;
+}
+
+define('ENV_FOR_WORDPRESS_VERSION', '1.0.0');
